4119defb3a9d68d86a2f59dbbc085f6f089829c7
joky-ka <joky-ka@68602f59-ab36-0410-8265-eed38e9a9012>
2007-10-01 07:06:16 +0000

    Project restart

--- trunk/includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("PROJECTNAME", "DistributedGallery");
-
-define('MYSQL_HOST',     'localhost');
-define('MYSQL_USER',     'distgal');
-define('MYSQL_PASS',     'distgal');
-define('MYSQL_DATABASE', 'distgal');
-?>
